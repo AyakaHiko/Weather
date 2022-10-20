85baf3c54b8c9f7108b86223c74445ab930240b1
Ayaka <m.rusakk@gmail.com>
2022-10-20 18:38:19 +0200

    Add project files.

---
+++ Weather/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cityBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.findBtn = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.temperatureInfo = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.humidityInfo = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pressureInfo = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cityBox
+            // 
+            this.cityBox.Location = new System.Drawing.Point(60, 16);
+            this.cityBox.Name = "cityBox";
+            this.cityBox.Size = new System.Drawing.Size(215, 20);
+            this.cityBox.TabIndex = 0;
+            this.cityBox.Text = "Mykolaiv";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "City";
+            // 
+            // findBtn
+            // 
+            this.findBtn.Location = new System.Drawing.Point(281, 16);
+            this.findBtn.Name = "findBtn";
+            this.findBtn.Size = new System.Drawing.Size(75, 23);
+            this.findBtn.TabIndex = 2;
+            this.findBtn.Text = "Find";
+            this.findBtn.UseVisualStyleBackColor = true;
+            this.findBtn.Click += new System.EventHandler(this.findBtn_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 51);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(360, 126);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.temperatureInfo);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(79, 24);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Temperature";
+            // 
+            // temperatureInfo
+            // 
+            this.temperatureInfo.AutoSize = true;
+            this.temperatureInfo.Location = new System.Drawing.Point(76, 11);
+            this.temperatureInfo.Name = "temperatureInfo";
+            this.temperatureInfo.Size = new System.Drawing.Size(0, 13);
+            this.temperatureInfo.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.humidityInfo);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(3, 33);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(79, 24);
+            this.panel2.TabIndex = 3;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // humidityInfo
+            // 
+            this.humidityInfo.AutoSize = true;
+            this.humidityInfo.Location = new System.Drawing.Point(76, 11);
+            this.humidityInfo.Name = "humidityInfo";
+            this.humidityInfo.Size = new System.Drawing.Size(0, 13);
+            this.humidityInfo.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Humidity";
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSize = true;
+            this.panel3.Controls.Add(this.pressureInfo);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(3, 63);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(79, 24);
+            this.panel3.TabIndex = 3;
+            // 
+            // pressureInfo
+            // 
+            this.pressureInfo.AutoSize = true;
+            this.pressureInfo.Location = new System.Drawing.Point(76, 11);
+            this.pressureInfo.Name = "pressureInfo";
+            this.pressureInfo.Size = new System.Drawing.Size(0, 13);
+            this.pressureInfo.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Pressure";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 191);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.findBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cityBox);
+            this.MaximumSize = new System.Drawing.Size(400, 230);
+            this.MinimumSize = new System.Drawing.Size(400, 230);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cityBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button findBtn;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label temperatureInfo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label humidityInfo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label pressureInfo;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Weather/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Weather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void findBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var data = WeatherServer.GetWeather(cityBox.Text);
+                if (InvokeRequired)
+                    Invoke(new Action(() => _fillWeather(data)));
+                else
+                {
+                 _fillWeather(data);   
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void _fillWeather(Weather data)
+        {
+            pressureInfo.Text = data.Pressure.ToString();
+            humidityInfo.Text = data.Humidity.ToString();
+            temperatureInfo.Text = data.Temperature.ToString();
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+    public class WeatherServer
+    {
+        private static string _apiKey = "8f3711470cbfa8043c11f3075e50619e";
+
+
+        private static string _request(string city)
+        {
+            string api = $@"https://api.openweathermap.org/data/2.5/weather?q={city}&mode=xml&units=metric&appid={_apiKey}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(api);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (var stream = response.GetResponseStream())
+            {
+                StringBuilder sb = new StringBuilder();
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        sb.AppendLine(line);
+                    }
+                }
+                return sb.ToString();
+            }
+        }
+
+        public static Weather GetWeather(string city)
+        {
+            var stringRequest = _request(city);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(stringRequest);
+            var root = doc.DocumentElement;
+            Weather weather= new Weather{City = city};
+            foreach (XmlNode node in root.ChildNodes)
+            {
+                var value = node?.Attributes["value"]?.Value;
+                switch (node.Name)
+                {
+                    case "temperature":
+                        weather.Temperature = Convert.ToDecimal(value);
+                        break;
+                    case "humidity":
+                        weather.Humidity = Convert.ToInt32(value);
+                        break;
+                    case "pressure":
+                        weather.Pressure = Convert.ToInt32(value);
+                        break;
+                }
+            }
+            return weather;
+        }
+    }
+
+    public class Weather
+    {
+        public string City { get; set; }
+        public decimal Temperature { get; set; }
+        public int Humidity { get; set; }
+        public int Pressure { get; set; }
+    }
+}
